dfcd5e82bb9dd4cd642e1de3b6b520db9937f839
Koichi Nishiyama <nishiyama4477@gmail.com>
2020-12-21 20:11:46 +0900

    enumerateを使わなくても、range(1, n)でs[i-1]を使えば、indexを扱えた。

--- 143.py
+++ 143.py
@@ -3,10 +3,14 @@ s = input()
 
 count = 1
 
-for i, slime in enumerate(s):
-    if i == len(s) -1:
-        break
-    if slime != s[i+1]:
+# for i, slime in enumerate(s):
+#     if i == len(s) -1:
+#         break
+#     if slime != s[i+1]:
+#         count += 1
+
+for i in range(1, n):
+    if s[i-1] != s[i]:
         count += 1
 
 print(count)
