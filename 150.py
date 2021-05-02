48d0ee8a749f70e59e631ee45f6a0db54edfeee5
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-05-02 16:05:45 +0900

    失敗。考えられるズレは,index値ｐのズレ。底を修正してみる。

--- 150.py
+++ 150.py
@@ -7,17 +7,15 @@ N = int(input())
 s1 = [int(i) for i in input().split()]
 s2 = [int(i) for i in input().split()]
 
-# p = index
+# p = index, q = value(今回はtuple）
 A = 0
 B = 0
-print("s1は", s1)
-print("s2は", s2)
-for p, q in enumerate(itertools.permutations(range(N))):
-    print("pは" , p)
-    print("qは", q)
-    # if q == s1:
-    #     A = p
-    # elif q == s2:
-    #     B = p
-print(A)
-print(B)
+
+for p, q in enumerate(itertools.permutations(range(1, N + 1))):
+    # print(list(q))
+    if list(q) == s1:
+        A = p
+    elif list(q) == s2:
+        B = p
+
+print(abs(A - B))
--- draft.py
+++ draft.py
@@ -1,6 +1,7 @@
 s1 = [1,2]
 p = (1,2)
-if s1 == p:
-    print('Yes')
-else:
-    print('No')
+
+n_p = list(p)
+
+print(p)
+print(n_p)
