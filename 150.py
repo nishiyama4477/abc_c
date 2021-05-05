b5fdfb1329d2a5b2c4f5beec1b0f5c16726e4f39
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-05-05 18:13:10 +0900

    メッセージを消した

--- 150.py
+++ 150.py
@@ -20,16 +20,9 @@ if s1 == s2:
     B = 0
 else:
     for p, q in enumerate(itertools.permutations(range(1, N + 1))):
-        # print(list(q))
         if list(q) == s1:
-            # print('s1は', s1)
-            # print('qは', list(q))
             A = p + 1
-            # print('Aは', A)
         elif list(q) == s2:
-            # print('s2は', s2)
-            # print('qは', list(q))
             B = p + 1
-            # print('Bは', B)
 
 print(abs(A - B))
