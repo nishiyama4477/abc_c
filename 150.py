782868ec5ca63089ce226d6070509e5d6dca9e25
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-05-05 18:12:11 +0900

    成功した。s1とs2が同じ時の処理を追加した。でももっと短い処理がある。

--- 150.py
+++ 150.py
@@ -8,14 +8,28 @@ s1 = [int(i) for i in input().split()]
 s2 = [int(i) for i in input().split()]
 
 # p = index, q = value(今回はtuple）
+# AとBはpermutationsのindex値
 A = 0
 B = 0
 
-for p, q in enumerate(itertools.permutations(range(1, N + 1))):
-    # print(list(q))
-    if list(q) == s1:
-        A = p + 1
-    elif list(q) == s2:
-        B = p + 1
+# errorが起きた原因は、
+# permutationsしたときに、かぶりがあるとか順列の問題、
+# もしくは端っこの値。
+if s1 == s2:
+    A = 0
+    B = 0
+else:
+    for p, q in enumerate(itertools.permutations(range(1, N + 1))):
+        # print(list(q))
+        if list(q) == s1:
+            # print('s1は', s1)
+            # print('qは', list(q))
+            A = p + 1
+            # print('Aは', A)
+        elif list(q) == s2:
+            # print('s2は', s2)
+            # print('qは', list(q))
+            B = p + 1
+            # print('Bは', B)
 
 print(abs(A - B))
--- draft.py
+++
@@ -1,7 +0,0 @@
-s1 = [1,2]
-p = (1,2)
-
-n_p = list(p)
-
-print(p)
-print(n_p)
