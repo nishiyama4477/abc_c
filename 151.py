2ed69c0563ead6dc218b08f15c6ddca0af2b5343
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-05-08 19:12:08 +0900

    参考1: まだ解読中

--- 151.py
+++ 151.py
@@ -3,24 +3,26 @@
 #         ACかつac_cntが0であればac += 1,wc += wc_cnt
 #         WCかつac_cntが0であればwc_cnt += 1
 
+
 n, m = map(int, input().split())
 
 sth = 'dammy'
 ac = 0
 wc = 0
 wc_cnt = 0
-ac_cnt = 0
+ac_exist = False
 
 for i in range(m):
     a, b = map(str, input().split())
+    # 問題が変わった時
     if a != sth:
         # print('switched!')
         wc_cnt = 0
-        ac_cnt = 0
+        ac_exist = False
         sth = a
 
         if b == 'AC':
-            ac_cnt += 1
+            ac_exist = True
             # print('ac_cnt += 1')
             ac += 1
             # print('ac += 1')
@@ -28,17 +30,18 @@ for i in range(m):
             wc_cnt += 1
             # print('wc_cnt += 1')
 
+    # 前と同じ問題の時
     else:
         if b == 'AC':
-            if ac_cnt == 0:
-                ac_cnt += 1
+            if ac_exist == False:
+                ac_exist = True
                 # print('ac_cnt += 1')
                 ac += 1
                 # print('ac += 1')
                 wc += wc_cnt
                 # print('wc += wc_cnt')
         else:
-            if ac_cnt == 0:
+            if ac_exist == False:
                 wc_cnt += 1
                 # print('wc_cnt += 1')
 
--- draft.py
+++ draft.py
@@ -1,1 +1,51 @@
 
+# import json
+# import base64
+#
+# data = {}
+# with open('https://machinelearningmastery.com/wp-content/uploads/2019/02/sample_image.png', mode='rb') as file:
+#     img = file.read()
+# data['img'] = base64.encodebytes(img).decode('utf-8')
+#
+# print(json.dumps(data))
+
+import sys
+
+N, M = map(int, input().split())
+A = []
+if M == 0:
+    print(0, 0)
+    sys.exit()
+else:
+    for _ in range(M):
+        a, b = input().split()
+        A.append([a, b])
+print(A)
+
+dp = [0] * (N + 1)
+dp2 = [0] * (N + 1)
+cnt_w = 0
+cnt_a = 0
+print(dp)
+print(dp2)
+# dpもdp2もACの時だけ＋１される
+# seq[0]は問題番号
+# 2周しているforを
+# ↓このループはACがあるところのdp2を1にする
+for seq in A:
+    if seq[1] == "AC":
+        dp2[int(seq[0])] = 1
+        print('dp2の' + seq[0] + 'が１になった')
+
+# このループは初のACを1にする かつ cnt_wが分からない?????
+
+for seq in A:
+    if seq[1] == "AC" and dp[int(seq[0])] == 0:
+        cnt_a += 1
+        print('cnt_aが+1された')
+        dp[int(seq[0])] = 1
+        print('dpの' + seq[0] + 'が１になった')
+    elif seq[1] == "WA" and dp[int(seq[0])] == 0 and dp2[int(seq[0])] == 1:
+        cnt_w += 1
+        print('cnt_wが+1された')
+print(cnt_a, cnt_w)
