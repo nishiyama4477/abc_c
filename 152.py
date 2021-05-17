820a1f49f9e3230249c87072e95c19f5667f798d
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-05-17 18:27:59 +0900

    TLEで失敗した。なぜだ

--- 152.py
+++ 152.py
@@ -2,8 +2,9 @@
 #      左の条件はP[:i]、右はPi <= Pj
 #      P[:i]の全てのカズはPi <= Pjを満たす必要がある。
 #      今回はPiを軸として「bench」とおき、P[:i]をpとし、
-#      for q in pでqがbenchを下回ったときは条件に合わないと判断した。
-#      全て満たせばcountを+
+#      min(p) < benchとなった場合はcount += 1とし、
+#      print(N - count)とした。
+
 N = int(input())
 P = [int(i) for i in input().split()]
 
@@ -12,14 +13,19 @@ count = 0
 for i, value in enumerate(P):
 
     bench = P[value - 1]
+    # print('benchは', bench)
     # print(value)
     p = P[:value]
+    # print('pは' ,p)
     # print(p)
     # print(bench)
     # ここでpの要素全てがbench以上でなければならない
 
-    for q in p:
-        if q < bench:
-            count += 1
+    # for q in p:
+    #     if q < bench:
+    #         count += 1
+    #         break
+    if min(p) < bench:
+        count += 1
 
 print(N - count)
