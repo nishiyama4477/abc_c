a84115dfae006bff11809965ca20fc68f81ca398
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-05-20 17:45:46 +0900

    成功！ポイントは ①： あるリスト(A-list)の要素全てがある数(B)以上である→BはA-listの最小値である。 ②： TLEが出たということは回る数を減らさないといけない。for enumerateよりはただのforの方が時間は短くなる。 ③： for i in range(N)にしたこと。ダブっている要素があるのなら、enumerateじゃなくてrange(N)のようにまとめて考えてみると以外といける。

--- 152.py
+++ 152.py
@@ -1,31 +1,27 @@
 # 仮説1: まずforループ
-#      左の条件はP[:i]、右はPi <= Pj
-#      P[:i]の全てのカズはPi <= Pjを満たす必要がある。
-#      今回はPiを軸として「bench」とおき、P[:i]をpとし、
+#      左の条件はP[:j:i]、右はPi <= Pj ← PjというのはP[:j:i]
+#      P[:j:i]の全てのカズはPi以上でないといけない。　　←PiというのはP[:j:i]の最小値ということ！！！
+#      今回はPiを軸として「bench」とおき、P[:j:i]をpとし、
 #      min(p) < benchとなった場合はcount += 1とし、
 #      print(N - count)とした。
 
 N = int(input())
 P = [int(i) for i in input().split()]
 
+# count = 0
+#
+# for i, value in enumerate(P):
+#     bench = P[value - 1]
+#     p = P[:value]
+#     if min(p) < bench:
+#         count += 1
+#
+# print(N - count)
+bench = P[0]
 count = 0
-
-for i, value in enumerate(P):
-
-    bench = P[value - 1]
-    # print('benchは', bench)
-    # print(value)
-    p = P[:value]
-    # print('pは' ,p)
-    # print(p)
-    # print(bench)
-    # ここでpの要素全てがbench以上でなければならない
-
-    # for q in p:
-    #     if q < bench:
-    #         count += 1
-    #         break
-    if min(p) < bench:
+for i in range(N):
+    if bench >= P[i]:
+        bench = P[i]
         count += 1
 
-print(N - count)
+print(count)
--- draft.py
+++ draft.py
@@ -8,30 +8,8 @@
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 #
 # print(json.dumps(data))
-import sys
-
-input = sys.stdin.readline
-N, M = map(int, input().split())
-PS = [input().strip().split() for i in range(M)]
-# PSは ['1', 'AC'), ('2', 'WA')~~~]みたいな感じ
-
-penalty = [0] * (N + 1)
-AC = [False] * (N + 1)
-
-ac = 0
-for p, s in PS:
-    p = int(p)
-    if s == 'AC':
-        if not AC[p]:
-            ac += 1
-            AC[p] = True
-    else:
-        if not AC[p]:
-            penalty[p] += 1
-# ここで
-#penaltyは[0,0,2,1]
-#ACは[0,0,1,1]みたいな感じ
-
-penalty = [p * a for p, a in zip(penalty, AC)]
-# ここでACフラグが1の所だけpenaltyを足したいが、そこでzipしてpenaltyとACの要素をひとつずつかけていくことで実現出来る。
-print(ac, sum(penalty))
+a = map(int, input().split())
+b = list(map(int, input().split()))
+    
+print(type(a))
+print(type(b))
