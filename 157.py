dba51567b19aa555cd6cd853f3c65139e280c33e
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-06-04 18:41:20 +0900

    qiitaを参考にしながら解けた。例外となる（今回は－1となる）条件は次の二つ 1：　2桁以上の時で先頭が0 2：　定義が二つ以上あるとき t = [-1] * nとしてダミーのベースを作った事、 2の条件をif t[s] = -1 and t[s] != cとしたこと、2桁目以降をfor i in range(1, n)と表現したことがポイントだったと思う

--- 157.py
+++ 157.py
@@ -4,18 +4,42 @@
 #   2: 定義が二つある（s=1 c=3, s=1 c=2など）
 
 n, m = map(int, input().split())
-N = []
-for i in range(n):
-    N.append(0)
-print(N)
+
+t = [-1] * n
 
 for i in range(m):
-    s, c = map(int, input().split())
-    print('sは', s)
-    print('cは', c)
-    if s == 1 and c == 0:
+    s,c = map(int, input().split())
+    # ここのsはindex値↓
+    s -= 1
+
+# このifは’定義が二つ以上あった場合'（上記の2）の処理。つまり①: なにか値が入っていて、②: かつさらに新しい値が入ってこようとしている時
+    if t[s] != -1 and t[s] != c:
         print(-1)
-        break
-    else:
-        N[s - 1] = c
+        exit()
+
+    t[s] = c
+
+# nが2桁以上の時は
+if n != 1:
+    # 先頭が0であればダメ
+    if t[0] == 0:
+        print(-1)
+        exit()
+    # 先頭が何も入っていなかったら1
+    if t[0] == -1:
+        t[0] = 1
+
+    # 先頭以外の値で何も入っていない時は0
+    for i in range(1, n):
+        if t[i] == -1:
+            t[i] = 0
+# 1桁の時何も入っていなかったら0、それ以外は0でも良いので放置
+else:
+    if t[0] == -1:
+        t[0] = 0
+
+print(''.join(map(str, t)))
+
+
+
 
--- draft2.py
+++ draft2.py
@@ -6,13 +6,38 @@
 # base64_message = base64_bytes.decode('ascii')
 #
 # print(base64_message)
-from collections import Counter
+N, M = map(int, input().split())
 
-_count = Counter()
-# print(_count)
-_count.update('Welcome to Guru99 Tutorials!')
-print(_count)
-print('%s : %d' % ('u', _count['u']))
-# print('\n')
-# for char in 'Guru':
-#     print('%s : %d' % (char, _count[char]))
+t = [-1] * N
+
+for _ in range(M):
+    s, c = map(int, input().split())
+    s -= 1
+    # 条件が二つ以上有るときつまり、①t[s]に先客がいる　かつ　②新規のｃである
+    if t[s] != -1 and t[s] != c:
+        print(-1)
+        exit()
+    t[s] = c
+
+
+# 2～3桁の場合は先頭が0だとダメ
+if N != 1:
+    if t[0] == 0:
+        print(-1)
+        exit()
+
+    if t[0] == -1:
+        t[0] = 1
+
+# 先頭以外で値が決まっていないものは0にする
+    for i in range(1, N):
+        if t[i] == -1:
+            t[i] = 0
+
+# 1桁の場合、入らなくても0のままいける。
+else:
+    if t[0] == -1:
+        t[0] = 0
+#
+print(t)
+# print(''.join(map(str, t)))
