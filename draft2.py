58391994f7d6207e9bcef0a0d227005dae824970
Koichi Nishiyama <nishiyama4477@gmail.com>
2021-06-12 21:48:39 +0900

    成功。いろいろ考えたあげくむちゃくちゃあっけなかった。 ポイントはlを3分割しそれぞれを掛けていったときの値が最大にする必要がある。それはlの平均なのでlを3で割った値を3乗すれば良い。

--- draft2.py
+++ draft2.py
@@ -7,42 +7,13 @@
 #
 # print(base64_message)
 
-import math
-
-a, b = map(int, input().split())
-
-# AとBは消費税抜きのそれぞれの値段
-A = a / 0.08
-B = b / 0.1
-
-print(A)
-print(B)
-
-if A == B:
-    print(A)
-elif A != B:
-    if A < B:
-        # Aを変えてBに寄せたとき
-        A = B
-        print('Aは', A)
-        print('Bは', B)
-        if math.floor(A * 0.08) == a:
-            print('route1')
-            print(B)
-    # Bを変えてAに寄せたとき
-    elif A > B:
-        B = A
-        print('Aは', A)
-        print('Bは', B)
-        if math.floor(B * 0.1) == b:
-            print('route2')
-            print(A)
-else:
-    print('失敗')
+l = int(input())
 
 
-# AとBをイコールにしないといけない
+base = l * 10
 
+value = base / 3
 
-# print(math.floor(B * 0.08))
-# print(math.floor(A * 0.1))
+ans = value ** 3 / 1000
+
+print(ans)
