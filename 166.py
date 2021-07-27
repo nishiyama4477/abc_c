4a0f0649ee5fb338820576369ddebcf3711da8bb
KoichiNishiyama <yappari114.339@icloud.com>
2021-07-27 18:13:24 +0900

    方針を立てて、コードをほぼイメージ通りに書いたがlist out of rangeのエラーが出る。検証は次回。

--- 166.py
+++ 166.py
@@ -1,4 +1,37 @@
+# 戦略はHsを元にしてループを回しながらそれぞれをMsと照らし合わせる。
+# この時、Hsの要素一つに対して、Msを全部回さないといけないのでおそらく２重のループ。
+
 n, m = map(int, input().split())
 Hs = [int(i) for i in input().split()]
 Ms = [list(map(int, input().split())) for i in range(m)]
+
+
+print(n)
+print(m)
+print('Hs is', Hs)
+print('Ms is ', Ms)
+
+counter = 0
+
+for i in range(1, len(Hs) + 1):
+    print(i, '回目', 'STARTED!!!')
+    print('i is ', i)
+    for j in Ms:
+        print('j is', j)
+        if i in j:
+            j.remove(i)
+            J = j[0]
+            print('J is', J)
+            print('Hs[i-1] is', Hs[i-1])
+            print('Hs[J-1] is', Hs[J-1])
+            if Hs[i-1] > Hs[J-1]:
+                counter += 1
+                print('プラスされたよ！')
+        #     print('q is', q)
+        # print('j is', j)
+        # print('Hs[i] is', Hs[i])
+
+print(counter)
+
+
 
--- draft2.py
+++ draft2.py
@@ -6,35 +6,13 @@
 # base64_message = base64_bytes.decode('ascii')
 #
 # print(base64_message)
+i = 2
+j = [1,2]
+p = [5,6,7]
+j.remove(i)
+print(j)
+print(j[0])
 
-N, M, Q = map(int, input().split())
-l = [list(map(int, input().split())) for l in range(Q)]
 
-import itertools
 
-t = [i for i in range(1, M + 1)]
-# ここでゆうAは、問題文のAで数自体はいindex値を表している。
-A = list(itertools.combinations_with_replacement(t, N))
-# tというのは、Mのrange、つまりAのそれぞれの桁のとりうる値。
-print('tは', t)
-# lというのは、Qのこと。
-print('lは', l)
-# Aというのは、そのままAの列挙。
-print('Aは', A)
-X = []
-for j in range(len(A)): # ここでAの全パターンについて検証していく。そのために、Aのindex値としてjをとる。
-  print('検証開始！')
-  print('jは', j)
-  c = 0
-  for k in range(Q): # それぞれのAに対してQつ分の条件について検証していく。
-    print('kは', k)
-    if A[j][l[k][1] - 1] - A[j][l[k][0] - 1] == l[k][2]: # 　これはAのbi - Aのai = cを表している。
-      # 少しややこしいが、Aは3数の列挙。
-      # A[j]は1 2 3。
-      # [l[k][1] - 1]はQのkつ目の2つ目。つまり、例題１でいうところの3をindex値3-2を使って取り出そうとしている。.
-      # A[j][l[k][1] - 1]はAのindex値2。
-      c += l[k][3]
-  X.append(c)
-print('Xは', X)
-# Xというのは可能性のあるAそれぞれの得点。
-print(max(X))
+
