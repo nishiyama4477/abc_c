a1d2f073563f24df9bf4f34c4acf9aea1936a271
KoichiNishiyama <yappari114.339@icloud.com>
2021-07-28 16:04:14 +0900

    エラーが起きている。list index out of range. 次はどこでそれが起きているか突き止める。

--- 166.py
+++ 166.py
@@ -1,6 +1,11 @@
 # 戦略はHsを元にしてループを回しながらそれぞれをMsと照らし合わせる。
 # この時、Hsの要素一つに対して、Msを全部回さないといけないのでおそらく２重のループ。
 
+# 手順を日本語で言うと、①それぞれのobservatoryからいくつobservatoryが繋がっているか。
+# ②その中で基準となるobservatoryが一番高かったor基準となるobservatoryしかないなら＋。
+
+# 今起きているエラー(list index out of range)は、iとjでループの回数が違うからだと思う。←肌感レベルだから信憑性はない。
+
 n, m = map(int, input().split())
 Hs = [int(i) for i in input().split()]
 Ms = [list(map(int, input().split())) for i in range(m)]
@@ -12,25 +17,25 @@ print('Hs is', Hs)
 print('Ms is ', Ms)
 
 counter = 0
-
+# Msがindex値なのでHsを取り出す時もindex値で取り出す。
 for i in range(1, len(Hs) + 1):
-    print(i, '回目', 'STARTED!!!')
-    print('i is ', i)
-    for j in Ms:
+    print(i, '個目', 'STARTED!!!')
+    for j in Ms: # Msはそのままでいい。なぜならMsはindexだから。
         print('j is', j)
         if i in j:
             j.remove(i)
-            J = j[0]
-            print('J is', J)
-            print('Hs[i-1] is', Hs[i-1])
-            print('Hs[J-1] is', Hs[J-1])
-            if Hs[i-1] > Hs[J-1]:
+            J = j[0] # このJはどちらが高いかの比較対象(いわばライバル。)のindex
+            print('Hs[i-1] is', Hs[i-1], '基準')
+            print('Hs[J-1] is', Hs[J-1], 'ライバル')
+            if Hs[i-1] <= Hs[J-1]:
+                # もしライバルに高さで負けたらbreakして次の基準にバトンタッチ。
+                break
+            elif j == Ms[m-1]:
                 counter += 1
-                print('プラスされたよ！')
-        #     print('q is', q)
-        # print('j is', j)
-        # print('Hs[i] is', Hs[i])
+            else:
+                continue
 
+#
 print(counter)
 
 
--- draft2.py
+++ draft2.py
@@ -6,13 +6,19 @@
 # base64_message = base64_bytes.decode('ascii')
 #
 # print(base64_message)
-i = 2
-j = [1,2]
-p = [5,6,7]
-j.remove(i)
-print(j)
-print(j[0])
+# i = 2
+# j = [1,2]
+# p = [5,6,7]
+# j.remove(i)
+# print(j)
+# print(j[0])
 
-
+for i in range(1,4):
+    print(i, 'iだよ')
+    for j in range(1,3):
+        if j == 2:
+            break
+        else:
+            print(j)
 
 
