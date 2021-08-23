3a343fe744b05ee6bc0af96019e6432a53f98651
KoichiNishiyama <yappari114.339@icloud.com>
2021-08-23 21:46:26 +0900

    他の人のコードの解読なう

--- draft_test.py
+++ draft_test.py
@@ -1,65 +1,41 @@
-# n = int(input())
-# Ns = [list(map(int, input().split())) for i in range(n)]
+n, m, x = map(int, input().split())
+A = [list(map(int, input().split())) for i in range(n)]
+c = [0] * n
+# おそらく習得度カウンター　n * m個になっている。言い換えれば本一つにつきm個の習得度カウンターがある。
+a = [[0] * m for i in range(n)]
+print("a is", a)
+
+# おそらく本の数だけ値段を取り出している。
+for i in range(n):
+    c[i] = A[i][0]
+
 #
-# print(Ns)
+for i in range(n):
+    for j in range(m):
+        print('a[i][j] is', a[i][j])
+        print('A[i][j + 1] is', A[i][j + 1])
+        a[i][j] = A[i][j + 1]
 
-from itertools import combinations
+print('for後のaは', a)
+inf = float('inf')
+ans = inf
 
-# sortedと仮定
-li2 = [[100, 3, 7, 5, 3, 1], [164, 4, 5, 2, 7, 8], [334, 7, 2, 7, 2, 9]]
+for i in range(2 ** n):
+    price = 0
+    comprehension = [0] * m
+    for j in range(n):
+        if (i >> j) & 1:
+            price += c[j]
+            flag = True
+            for k in range(m):
+                comprehension[k] += a[j][k]
+                if comprehension[k] < x:
+                    flag = False
+            if flag:
+                ans = min(ans, price)
 
-t = list(combinations(li2, 2))
-
-# print(t)
-#
-# for i in t:
-#     print(i)
-
-x = 10
-
-sample = ([50, 2, 3, 9],)
-
-sample2 = ([60, 2, 2, 4], [70, 8, 7, 9])
-
-sample3 = ([50, 2, 3, 9], [60, 2, 2, 4], [70, 8, 7, 9])
-
-# 出したい結果はsample2だったら[10, 9, 13]みたいな感じ。
-
-
-print(len(sample))
-# jが一つの時
-if len(sample) == 1:
-    a = sample[0][1:]
-    print(a)
-
-print(len(sample2))
-# jが2つの時
-if len(sample2) == 2:
-    a = sample2[0][1:]
-    b = sample2[1][1:]
-
-    c = [n + o for n, o in zip(a, b)]
-
-    print(c)
-
-print(len(sample3))
-# jが3つの時
-if len(sample3) == 3:
-    a = sample3[0][1:]
-    b = sample3[1][1:]
-    c = sample3[2][1:]
-
-    d = [n + o + p for n, o, p in zip(a, b, c)]
-
-    print(d)
-
-
-
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = [7, 8, 9]
-d = [x + y + z for x, y, z in zip(a, b, c)]
-
-# print(d)
-
+if ans == inf:
+    print(-1)
+else:
+    print(ans)
 
