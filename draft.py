9390924990932dc3d8744a1544f9a865ed1acf25
KoichiNishiyama <yappari114.339@icloud.com>
2021-10-11 22:35:04 +0900

    WAが出てしまった。。

--- draft.py
+++ draft.py
@@ -1,4 +1,4 @@
-import itertools
+
 # import json
 # import base64
 #
@@ -6,6 +6,10 @@ import itertools
 # with open('https://machinelearningmastery.com/wp-content/uploads/2019/02/sample_image.png', mode='rb') as file:
 #     img = file.read()
 # data['img'] = base64.encodebytes(img).decode('utf-8')
+
+import itertools
+from operator import mul
+from functools import reduce
 
 n = int(input())
 
@@ -26,13 +30,39 @@ def prime_factorizer(num):
         li.append(num)
     return li
 
-print(prime_factorizer(n))
+p = prime_factorizer(n)
+# print(p)
 
 # 素因数分解された後のlistの過半数の組み合わせを取り出し、その組み合わせを掛け合わしたもの
 # で一番数が小さくなるやつがx軸（もしくはy軸）になる.
 # 例えば112は[2,2,2,2,7]なので片方は3つ。3つの組み合わせの中で一番小さくなるのは222=8なので
 # 答えは8, 14(2*7）になる、、、、はず、、、
-combinations = itertools.combinations(prime_factorizer(n), -(-len(prime_factorizer(n)) // 2))
+combinations = list(itertools.combinations(prime_factorizer(n), -(-len(prime_factorizer(n)) // 2)))
+
+# print('過半数は', -(-len(prime_factorizer(n)) // 2))
+
+# leastは掛け合わせた時の最小の組み合わせ。掛け算はする前で[2,2,3]みたいな状態。
+least = combinations[0]
+
+# print('combinationsは,', combinations)
+# print('最初のcombinationsは', least)
 
 for i in combinations:
-    selected =
+    selected = reduce(mul, i)
+    if selected < reduce(mul, least):
+        # print('更新されたお')
+        least = i
+
+# print('leastは', least)
+
+for i in least:
+    p.remove(i)
+
+# print('removeされたpは', p)
+
+# ここでエラー。このpが空っぽなので。つまり、素因数分解した時に要素が一つしかないパターン。
+if not p:
+    print(n - 1)
+else:
+    print(reduce(mul, least) + reduce(mul, p) - 2)
+
