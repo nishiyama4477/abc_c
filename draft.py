cebb620d11089ca1e9966f242b894edeed216aaf
KoichiNishiyama <yappari114.339@icloud.com>
2021-10-27 14:09:20 +0900

    ちょっと埒があかないので、解説をみることにする。

--- draft.py
+++ draft.py
@@ -6,67 +6,18 @@
 # with open('https://machinelearningmastery.com/wp-content/uploads/2019/02/sample_image.png', mode='rb') as file:
 #     img = file.read()
 # data['img'] = base64.encodebytes(img).decode('utf-8')
-import math
-import itertools
-
-n = int(input())
-
-li = [input().split() for i in range(n)]
-
-fac = list(itertools.permutations(li))
-
-# print('それぞれの点は', li)
-
-# print('道順の組み合わせは', fac)
-
-# print('組み合わせのサンプルは', fac[0])
-
-
-total = 0
-
-for j in fac:
-# ひとつのパターンを使ったパターン
-    sample_sum = 0
-    for i, val in enumerate(j):
-        if i == len(j) - 1:
-            break
-        else:
-            val = list(int(i) for i in val)
-            val2 = list(int(i) for i in j[i + 1])
-            # print('地点Aは', val)
-            # print('地点Bは', val2)
-
-            zipped = zip(val, val2)
-            s = [x-y for x, y in zipped]
-            # print('sum は', s)
-            ans = math.sqrt(sum([i**2 for i in s]))
-            # print('ans　は', ans)
-            sample_sum += ans
-    total += sample_sum
-
-
-print(total / len(fac))
 
 
 
 
+# 方針１
+# まず、インパクトが大きいのはAの値。例えば、n = 100 のとき、1 * 100 > 1 * 3になる。
+# てことは、A * ○ の値が許容範囲を超えないかでifを組める説。
 
+a, b, x = map(int, input().split())
 
-# # ひとつのパターンを使ったパターン
-# sample_sum = 0
-# for i, val in enumerate(fac[0]):
-#     if i == len(fac[0])- 1:
-#         break
-#     else:
-#         val = list(int(i) for i in val)
-#         val2 = list(int(i) for i in fac[0][i + 1])
-#         print(val)
-#         print(val2)
-#
-#         zipped = zip(val, val2)
-#         s = [x+y for x, y in zipped]
-#         print('sum は', s)
-#         ans = math.sqrt(sum([i**2 for i in s]))
-#         print('ans　は', ans)
-#         sample_sum += ans
-# print(sample_sum)
+#Nは１以上なのでa+bがxよりも大きい時点でたとえ最小のInteger１が入ってきても買えない。
+if a + b > x:
+    print(0)
+    exit()
+else:
