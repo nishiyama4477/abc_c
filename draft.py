c335837d40054b7ba13a34ea29be5c81f6d64398
KoichiNishiyama <yappari114.339@icloud.com>
2021-11-02 15:15:06 +0900

    とりあえずbit全探索で列挙までは出来た。あとは列挙した全てのパターンについてどう検証するか

--- draft.py
+++ draft.py
@@ -21,21 +21,66 @@ for i in range(n):
     x_y = []
     for j in range(a):
         x, y = map(int, input().split())
-        x_y.append(x)
-        x_y.append(y)
+        pare = [x, y]
+        x_y.append(pare)
         print('証言', j+1,  'は', x, y)
     testimonies.append(x_y)
 
 print('testimonies is', testimonies)
 
-for i in range(1, n+1):
-    li = itertools.combinations(testimonies, i)
 
-    comb = list(li)
-    print(comb)
-    print('取り出し方の種類は', len(comb))
-    print(comb[0])
-    for j in comb:
-        count = [0] * n
-        count[]
+
+
+# 一旦保留↓ とりまbit全探索でやってみる
+#
+#
+# for i in range(1, n+1):
+#     li = itertools.combinations(testimonies, i)
+#
+#     comb = list(li)
+#     print(comb)
+#     print('取り出し方の種類は', len(comb))
+#     print(comb[0])
+#     for j in comb:
+#         count = [0] * n
+#         count[]
+
+#bit全探索でやってみる
+# money = 300
+# item = [('みかん', 100), ('りんご', 200), ('ぶどう', 300)]
+#
+# num = len(item)
+#
+# print(num)
+#
+#
+# for i in range(2**num):
+#     bag = []
+#     for j in range(num):
+#         if(i >> j) & 1:
+#             bag.append(item[j][0])
+#     print(bag)
+
+
+for i in range(2**n):
+    peo = []
+    for j in range(n):
+        if (i >> j) & 1:
+            peo.append(testimonies[j])
+    print('パターン', i, 'は', peo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
