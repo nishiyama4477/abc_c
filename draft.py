c7b5710ad274c94cf9fff1d0d6e9226dd40d1189
KoichiNishiyama <yappari114.339@icloud.com>
2021-11-06 16:16:58 +0900

    list index out of range になる。なぜか。次検証したい。

--- draft.py
+++ draft.py
@@ -17,16 +17,16 @@ testimonies = []
 
 for i in range(n):
     a = int(input())
-    print('A is', a)
+    # print('A is', a)
     x_y = []
     for j in range(a):
         x, y = map(int, input().split())
         pare = [x, y]
         x_y.append(pare)
-        print('証言', j+1,  'は', x, y)
+        # print('証言', j+1,  'は', x, y)
     testimonies.append(x_y)
 
-print('testimonies is', testimonies)
+# print('testimonies is', testimonies)
 
 
 
@@ -60,7 +60,7 @@ print('testimonies is', testimonies)
 #             bag.append(item[j][0])
 #     print(bag)
 
-count = 0
+count = -float('inf')
 
 for i in range(2**n):
     peo = []
@@ -69,21 +69,35 @@ for i in range(2**n):
             peo.append(testimonies[j])
     # ここでパターンの取得は終わっているのでそれぞれが合っているか確認。
     # フラグとしては正直者と言われている人の発言に矛盾が生じたらアウト。矛盾とは同じ人に1と0宣言されること。
-    print('パターン', i, 'は', peo)
+    # print('パターン', i, 'は', peo)
     flag = [0] * n
+    # print('len(peo) is', len(peo))
     for k in peo:
-        print('k is', k[0])
-        if k[0][1] == 1 and k[0][1] >= 0:
+        # print('k is', k[0])
+        # 前　嘘つき　今　正直者
+        if k[0][1] == 1 and flag[k[0][0]] < 0:
+            # print('course: 1')
+            flag[k[0][0]] = 'w'
+        # 前　正直　今　嘘つき
+        elif k[0][1] == 0 and flag[k[0][0]] > 0:
+            # print('course: 2')
+            flag[k[0][0]] = 'w'
+        elif k[0][1] == 1 and k[0][1] >= 0 and flag[k[0][0]] is not type(str):
+            # print('course: 3')
             flag[k[0][0]] += 10
-        elif k[0][1] == 0 and k[0][1] <= 0:
+        elif k[0][1] == 0 and k[0][1] <= 0 and flag[k[0][0]] is not type(str):
+            # print('course: 4')
             flag[k[0][0]] -= 10
         # 矛盾が発生した時（上記のifでもelifでもない時）、もしくはパターンとして正直者が1人もいない（peoの要素が0の時。
         # これはもう少し上で場合分けしたが良さそう）の場合分けが出来ていない。
-        else:
-            print('NOOO')
-            break
 
-    print('flag', flag)
+    # print('flag', flag)
+    if 'w' not in flag:
+        # print('CONGRATULATION, COUNTED!!')
+        # print('___________________')
+        # print('___________________')
+        if len(peo) > count:
+            count = len(peo)
 
 
 print(count)
