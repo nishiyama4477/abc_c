42a0dbafa0864b1ae096c0ab5b30f36c72c8be83
KoichiNishiyama <yappari114.339@icloud.com>
2021-11-20 16:39:35 +0900

    コード解読なう。新発見として、 •bit全探索で使う2**nと1<<nは一緒だ、と言うことが分かった！！

--- draft.py
+++ draft.py
@@ -7,72 +7,6 @@
 #     img = file.read()
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 
-# 方針１
-# N人の取り出し方それぞれに対して検証する。大きい数からやっていけば検証が通った途端にそれが答えとなる。
-# import itertools
-
-# n = int(input())
-#
-# testimonies = []
-#
-# for i in range(n):
-#     a = int(input())
-#     # print('A is', a)
-#     x_y = []
-#     for j in range(a):
-#         x, y = map(int, input().split())
-#         pare = [x, y]
-#         x_y.append(pare)
-#         # print('証言', j+1,  'は', x, y)
-#     testimonies.append(x_y)
-#
-# print('testimonies is', testimonies)
-#
-# count = -float('inf')
-#
-# for i in range(2**n):
-#     peo = []
-#     for j in range(n):
-#         if (i >> j) & 1:
-#             peo.append(testimonies[j])
-#     # ここでパターンの取得は終わっているのでそれぞれが合っているか確認。
-#     # フラグとしては正直者と言われている人の発言に矛盾が生じたらアウト。矛盾とは同じ人に1と0宣言されること。
-#     print('--------------------')
-#     print('パターン', i, 'は', peo)
-#     flag = [0] * n
-#     print('flag is', flag)
-#     print('正直者の数は', len(peo))
-#     for k in peo:
-#         print('k is', k[0])
-#         # 前　嘘つき　今　正直者
-#         if k[0][1] == 1 and flag[k[0][0]] < 0:
-#             print('course: 1')
-#             flag[k[0][0]] = 'w'
-#         # 前　正直　今　嘘つき
-#         elif k[0][1] == 0 and flag[k[0][0]] > 0:
-#             print('course: 2')
-#             flag[k[0][0]] = 'w'
-#         # 正直者かつflagがwではない
-#         elif k[0][1] == 1 and k[0][1] >= 0 and flag[k[0][0]] is not type(str):
-#             print('course: 3')
-#             flag[k[0][0]] += 10
-#         # 嘘つきかつflagがwではない
-#         elif k[0][1] == 0 and k[0][1] <= 0 and flag[k[0][0]] is not type(str):
-#             print('course: 4')
-#             flag[k[0][0]] -= 10
-#         # 矛盾が発生した時（上記のifでもelifでもない時）、もしくはパターンとして正直者が1人もいない（peoの要素が0の時。
-#         # これはもう少し上で場合分けしたが良さそう）の場合分けが出来ていない。
-#
-#     # print('flag', flag)
-#     if 'w' not in flag:
-#         # print('CONGRATULATION, COUNTED!!')
-#         # print('___________________')
-#         # print('___________________')
-#         if len(peo) > count:
-#             count = len(peo)
-#
-#
-# print(count)
 
 n = int(input())
 
@@ -90,9 +24,7 @@ for i in range(n):
     testimonies.append(x_y)
 
 print('testimonies is', testimonies)
-# print(len(testimonies))
-
-
+print(len(testimonies))
 
 for i in range(2**n):
     #peoはパターンを表す。
@@ -105,6 +37,11 @@ for i in range(2**n):
     print('--------------------')
     print('パターン', i, 'は', peo)
     print('要素の数は', len(peo))
+    for a in peo:
+        print(a)
+
+
+
 
 
 
--- sample.py
+++ sample.py
@@ -1,16 +1,36 @@
-# n = int(input())
+N = int(input())
+
+A_list = []
+# 証言者ごとに集めた証言のリスト
+xy_list = []
+# 矛盾がなかった回答のカウンター
+ans = 0
 #
-# testimonies = []
-#
-# for i in range(n):
-#     a = int(input())
-#     # print('A is', a)
-#     x_y = []
-#     for j in range(a):
-#         x, y = map(int, input().split())
-#         pare = [x, y]
-#         x_y.append(pare)
-#         # print('証言', j+1,  'は', x, y)
-#     testimonies.append(x_y)
-#
-# print(testimonies)
+for i in range(N):
+    A = int(input())
+    xy = [list(map(int, input().split())) for _ in range(A)]
+    xy_list.append(xy)
+
+# 1<<Nと2**Nは一緒。
+for st in range(1 << N):
+    for witness, test in enumerate(xy_list):
+        if not st >> witness & 1:
+            continue
+
+        for person, testimony in test:
+            # 0-index
+            person -= 1
+            if st >> person & 1 != testimony:
+                break
+
+        else:
+            continue
+
+        break
+
+    else:
+        temp_r = bin(st).count('1')
+        if temp_r > ans:
+            ans = temp_r
+
+print(ans)
