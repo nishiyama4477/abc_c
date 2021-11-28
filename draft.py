2ee29d0f67449bdd818f7b6805b8f71ebf8a8eef
KoichiNishiyama <yappari114.339@icloud.com>
2021-11-28 15:36:31 +0900

    A_primesとB_primesの非共通の要素をuncommonとして取り出して、A_primesでループを回してiがuncommonにあったらB_primesに足すというプログラムを書いたがWAがひとつあった。

--- draft.py
+++ draft.py
@@ -6,6 +6,9 @@
 #     img = file.read()
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 
+
+# ABC148
+# A_primesとB_primesのconcatenateするときのポイントとして、二つに共通する因数は消去して良い。片方にしかない因数は消去してはならない。
 import math
 from operator import mul
 from functools import reduce
@@ -36,10 +39,14 @@ B_primes = prime_fact(b)
 #
 # print(prime_fact(18696))
 
+uncommon = set(A_primes) ^ set(B_primes)
+
 for i in A_primes:
-    if i not in B_primes:
+    if i in uncommon:
         B_primes.append(i)
-#
-# print('最終的なリストは', B_primes)
+
+
+# print(B_primes)
+
 print(reduce(mul, B_primes))
 
