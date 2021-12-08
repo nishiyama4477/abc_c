972240717b98286da4e3f8908bde5b339be9a218
KoichiNishiyama <yappari114.339@icloud.com>
2021-12-08 23:09:40 +0900

    ABC148はただ単に最小公倍数を求める問題で、ユークリッドの互除法を使えば出来た。ABC149だが、is_primeに問題がある可能性がある。

--- draft.py
+++ draft.py
@@ -7,49 +7,28 @@
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 
 
-# ABC148
-# A_primesとB_primesのconcatenateするときのポイントとして、二つに共通する因数は消去して良い。片方にしかない因数は消去してはならない。
-import math
-from operator import mul
-from functools import reduce
+# ABC149
+
+x = int(input())
 
 
-def prime_fact(n):
-    p_numbers = []
-    while n % 2 == 0:
-        p_numbers.append(2)
-        n = n // 2
-    for i in range(3, int(math.sqrt(n)+1), 2):
-        while n % i == 0:
-            p_numbers.append(i)
-            n = n // i
-    if n > 2:
-        p_numbers.append(n)
-
-    return p_numbers
+def is_prime(n):
+    flag = True
+    if n > 1:
+        for i in range(2, n):
+            if x % i == 0:
+                flag = False
+                break
+    if flag:
+        return False
+    else:
+        return True
 
 
-a, b = map(int, input().split())
-
-A_primes = prime_fact(a)
-B_primes = prime_fact(b)
-
-print('A_primes is', A_primes)
-print('B_primes is', B_primes)
-
-# print(prime_fact(18696))
-
-uncommon = set(A_primes) ^ set(B_primes)
-
-for i in A_primes:
-    if i in uncommon:
-        B_primes.append(i)
-
-
-print(B_primes)
-
-print(reduce(mul, B_primes))
-
-
+for i in range(x, 10**5+1):
+    print(i)
+    if is_prime(i):
+        print(i)
+        quit()
 
 
