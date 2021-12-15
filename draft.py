a40ebc6e04c510b82bf11cec86721c32b7f9a20d
KoichiNishiyama <yappari114.339@icloud.com>
2021-12-15 19:52:02 +0900

    失敗。WAが３分の１くらいある。やはり効率が悪い。次は解答を見ようと思う。

--- draft.py
+++ draft.py
@@ -14,48 +14,58 @@ n, m = map(int, input().split())
 ans = [list(input().split()) for i in range(m)]
 
 if m == 0:
-    print(0)
+    print(0, 0)
     quit()
 
-print(ans)
+# print(ans)
 
 solved = []
 q = int(ans[0][0])
-print(q)
+# print(q)
 
 ac = 0
 wa = 0
 
 penalties = 0
 
+# print('初期値は', q)
 
 for index, i in enumerate(ans):
-    print('○始まるよ○')
+    # print('○始まるよ○')
     # 新しい問題だ！！
+    if q != int(i[0]):
+        # print('new problem appears!')
+        wa += penalties
+        penalties = 0
+        q = int(i[0])
+        # print('決算終了したよ。acは', ac, 'waは', wa, 'penaltiesは', penalties)
+
     if i[1] == 'WA' and q not in solved:
-        print('まだ正解してないよ。だからwaを足したよ')
+        # print('まだ正解してないよ。だからwaを足したよ')
         penalties += 1
 
     if i[1] == 'AC' and q not in solved:
-        print('正解した問題が追加されたよ')
+        # print('正解した問題が追加されたよ')
         solved.append(q)
         ac += 1
 
-    if index != len(ans) - 1 and q != int(ans[index + 1][0]):
-        print('new problem appears!')
+    if index == len(ans) - 1:
         wa += penalties
-        penalties = 0
-        q = int(ans[index+1][0])
-        print('決算終了したよ。acは', ac, 'waは', wa, 'penaltiesは', penalties)
 
-print('solved is', solved)
+    # print('ac is', ac)
+    # print('wa is', wa)
+    # print('penalties is', penalties)
+
+
+
+# print('solved is', solved)
 
 
 
 
 # corect answersの数はsolvedの長さに等しいのでlen(solved)で表せる。
-print(len(solved))
-print(wa)
+print(ac, wa)
+
 
 
 
