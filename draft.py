ed568f1cef53c1ab321ed09eb4a0cd9b07cadf9d
KoichiNishiyama <yappari114.339@icloud.com>
2022-01-06 14:55:40 +0900

    WAが５個あった。どこかが違う。

--- draft.py
+++ draft.py
@@ -7,56 +7,20 @@
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 
 
-# ABC155
-# ①１番多く登場したやつを探す。
-# ②それをlexicographicalに出力する。
-# TLEを防ぐにはたぶんforループ一回で済まさないといけない。
+# ABC156
+# 仮定①：これは与えられた数n個の平均が目的地になる。2.5などの時はおそらくどっちでもいいか、どちらか。
+# なのでまずは切り捨てで作ってみる。
 
 n = int(input())
 
-words = []
-counter = []
+people = list(map(int, input().split()))
 
-for i in range(n):
-    # print('ループ', i+1, '回目')
-    word = input()
-    if word not in words:
-        words.append(word)
-        counter.append(1)
-    else:
-        where = words.index(word)
-        counter[where] += 1
+place = -(-sum(people) //  len(people))
 
-    # print('現時点でのwords', words)
-    # print('現時点でのcounter', counter)
+ans = 0
 
+for i in people:
+    point = (i - place) ** 2
+    ans += point
 
-# print(words)
-# print(counter)
-
-# zipped = list(zip(words, counter))
-#
-# print(zipped)
-#
-# zipped2 = sorted(zipped)
-#
-# print(zipped2)
-#
-# most = sorted(counter, reverse=True)[0]
-
-sample_list = sorted(list(zip(counter, words)), reverse=True)
-# print(sample_list)
-
-most = list(sample_list[0])[0]
-answer = []
-
-for i, v in sample_list:
-    if i == most:
-        answer.append(v)
-    else:
-        break
-
-answer.sort()
-
-for i in answer:
-    print(i)
+print(ans)
