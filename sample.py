c499a6184db964acf16cb24bed442192a57620f2
KoichiNishiyama <yappari114.339@icloud.com>
2022-01-12 16:03:03 +0900

    解けた。場合分けと、リストから整数を作るときにjoinを使ったことがポイント。

--- sample.py
+++ sample.py
@@ -9,7 +9,8 @@ for i in range(m):
     # インデックスのために-1
     s -= 1
 
-    # 予想がダブったらprint(-1)
+    # 予想が二つ出来たらprint(-1)。同じ予想だったらそのまま続行。
+    # t[s] != -1というのは値が入っていないということ。
     if t[s] != -1 and t[s] != c:
         print(-1)
         exit()
@@ -17,6 +18,37 @@ for i in range(m):
     t[s] = c
 
 print(t)
+
+# ２桁いじょうの時
+if n != 1:
+    # ２桁以上の整数で最初の桁の予想が0の場合もある。なぜなら0<=c<=9だから。
+    if t[0] == 0:
+        print(-1)
+        exit()
+    if t[0] == -1:
+        t[0] = 1
+
+    for i in range(1, n):
+        if t[i] == -1:
+            t[i] = 0
+else:
+    if t[0] == -1:
+        t[0] = 0
+
+print(''.join(map(str, t)))
+
+t = [-1] * n
+
+for i in range(m):
+    pre, val = map(int, input().split())
+    pre -= 1
+    if t[pre] != -1 and t[pre] != val:
+        print(-1)
+        exit()
+
+    t[pre] = val
+
+# print(t)
 
 if n != 1:
     if t[0] == 0:
@@ -31,5 +63,6 @@ if n != 1:
 else:
     if t[0] == -1:
         t[0] = 0
+# print(t)
 
 print(''.join(map(str, t)))
