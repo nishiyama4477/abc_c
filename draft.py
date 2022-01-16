f86c21128f2584e1eb39b3c4cd17b1f899df29c4
KoichiNishiyama <yappari114.339@icloud.com>
2022-01-16 14:20:23 +0900

    成功！近年稀にみるほどの簡単さ。

--- draft.py
+++ draft.py
@@ -7,34 +7,18 @@
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 
 
-# ABC158
-# 仮説①：forで回す。が、範囲は最小から最大まで決められるはず。例えば、a=2、b=2のとき、
-# 最小はx = 2.9999 / 0.08、最大はx = 2/0.1 の時。この範囲に消費税10%があれあ出力する。
-# ポイントは四捨五入ではなく切り捨て。　
-import math
-a, b = map(int, input().split())
-
-mi8 = a / 0.08
-ma8 = (a + 0.99) / 0.08
-
-# print('mi8 is', mi8)
-# print('ma8 is', ma8)
-# print('確認：', mi8 * 0.08)
-# print('確認：', ma8 * 0.08)
-
-mi10 = b / 0.10
-ma10 = (b + 0.99) / 0.10
-
-# print('mi10 is', mi10)
-# print('ma10 is', ma10)
-# print('確認：', mi10 * 0.10)
-# print('確認：', ma10 * 0.10)
-
-for i in range(int(mi10), int(ma10) + 1):
-    # print('i is', i)
-    if math.floor(i * 0.08) == a:
-        print(i)
-        exit()
-print(-1)
+# ABC159
+# 仮説①：Lをいかにして３桁の掛け算にするか、それらを掛け合わせて出来る数を最大化させる。
+# ポイントは３で割り切れない数がきた時。
 
 
+l = int(input())
+
+l3 = l // 3
+# actl3 = l / 3
+
+# print(l3)
+# print(actl3)
+
+print(l3**3)
+# print(actl3**3)
