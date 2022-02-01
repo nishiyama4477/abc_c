e41fafc8229ba769aa56707c947cbcf3c2ad5551
KoichiNishiyama <yappari114.339@icloud.com>
2022-02-01 14:06:07 +0900

    一応正解。だが、正攻法はユークリッドの互除法により処理を軽減させることである。このコードもぎりぎり感はあった。

--- draft.py
+++ draft.py
@@ -9,49 +9,25 @@
 
 # ABC162
 # 仮説１：取り出すlistを作ってあげて、そこからitertoolsで取り出す。
-import itertools
-import random
+# 解法はユークリッドの互除法を使うことだった！！
+# とりまgcdを使った後残りの２数に対してユークリッドしてみようと思う。
 import math
 
 k = int(input())
 
-counts = 0
+sum = 0
 
-comb = []
-
-# 2回目
-for i in range(1, k+1):
+for i in range(1,k+1):
     for j in range(1, k+1):
+        i_j = math.gcd(i,j)
         for l in range(1, k+1):
-            gcd1 = math.gcd(i, j)
-            gcd2 = math.gcd(j, l)
-            num = math.gcd(gcd1, gcd2)
-            counts += num
+            num = math.gcd(i_j, l)
+            sum += num
 
-print(counts)
-# base = []
-#
-# for i in range(3):
-#     num = []
-#     for j in range(1, k+1):
-#         num.append(j)
-#     base.append(num)
-#
-# print(base[0])
-#
-# ans = 0
-#
-# for i in range(1, k+1):
-#     a = random.choice(base[0])
-#     b = random.choice(base[1])
-#     c = random.choice(base[2])
-#     added = sum([a, b, c])
-#     ans += added
-#     base[0].remove(a)
-#     base[1].remove(b)
-#     base[2].remove(c)
-#
-# print(ans)
+print(sum)
+
+
+
 
 
 
