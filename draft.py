de1a0c99523e4d9ca2fc2d06974381022e776f60
KoichiNishiyama <yappari114.339@icloud.com>
2022-02-12 11:18:50 +0900

    ちょっと問題の構造が複雑。でもまあいけるのではないか？

--- draft.py
+++ draft.py
@@ -7,15 +7,19 @@
 # data['img'] = base64.encodebytes(img).decode('utf-8')
 
 
-# ABC164
+# ABC165
+# 仮説１
+# ステップを分けると、①Aのとりうる値を決める。②点数を判定し、より大きければ更新。←でも多分①の段階でおおよそのとりうる値的なのは見つけられそう。
+# というより、Asからある程度数字を予測できそうな気もする。=cになるようにAを作れば良いだけ。
+n, m, q = map(int, input().split())
 
-n = int(input())
+As = []
 
-items = list(input() for i in range(n))
+for s in range(q):
+    a = list(int(i) for i in input().split())
+    As.append(a)
 
-kind = len(set(items))
-
-print(kind)
+print(As)
 
 
 
