1fd22a7f1ed9fcb5fed8d0de3ccae26643336030
KoichiNishiyama <yappari114.339@icloud.com>
2022-03-11 13:52:10 +0900

    iが大きくなるにつれて、xの回数が小さくなるのは分かった。まだ解読なう。

--- sample.py
+++ sample.py
@@ -7,6 +7,11 @@ dat = [list(map(int, input().split())) for _ in range(Q)]
 ans = 0
 print('dat is', dat)
 
+# これはdfs時にifに引っかかった時だけに作用するfunc
+# dfsで出てきたAをquadsで点数づけ
+# ここでいうnumは点数
+
+
 def cal(p):
     num = 0
     for a, b, c, d in dat:
@@ -15,19 +20,31 @@ def cal(p):
     return num
 
 
+# ここではp=Aとして値を取り出し、その都度点数を更新する
+
+A_counter = 0
 
 
 def dfs(num, p=[]):
     global ans
+    global A_counter
+    global counter
     if len(p) == N:
+        print('ーーーーーーーーーーー')
+        A_counter += 1
         ans = max(ans, cal(p))
         return
+    # ここでAを作る。
     for x in range(num, M + 1):
+        print('dfs_stareted!', 'x = ', x)
+        counter += 1
         dfs(x, p + [x])
 
 
 for i in range(1, M + 1):
-    print('started!')
+    counter = 0
+    print('started!', 'i = ', i)
     dfs(i)
 
+print(A_counter)
 print(ans)
